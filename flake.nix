8aff38220b08d9a3e7dcaef549ed2eb8cffaa808
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-26 13:39:24 +0100

    Add possibility to ignore a repo
    When having a .f4s-update.json in the to-be-updated flake repository
    with content like e.g.
    {
      "ignore": {
        "repo": ["ignore-this-repo"]
      }
    }
    then f4s-update will not update inputs from the repo ignore-this-repo.

--- flake.nix
+++ flake.nix
@@ -9,11 +9,18 @@
       nix_bin = pkgs.nixFlakes + /bin/nix;
       jq_bin = pkgs.jq + /bin/jq;
 
+      jq_base_config = {
+        ignore = {
+          repo = [ ];
+        };
+      };
+
       jq_input_with_repo = ''
         .nodes as $nodes |
         reduce ($nodes.root.inputs | keys)[] as $input
           ([];
             if null == $nodes[$input].original.repo
+            or true == ($config.ignore.repo | contains([$nodes[$input].original.repo]))
             then .
             else . + ["--update-input " + $input]
             end) |
@@ -26,8 +33,13 @@
         program = (pkgs.writeScriptBin "update-repo-inputs.sh" ''
           #!${pkgs.stdenv.shell}
           set -e
+          config='${builtins.toJSON jq_base_config}'
           [[ ! -f flake.nix ]] && exit 0
-          params=$(${nix_bin} flake list-inputs --json | ${jq_bin} -r '${jq_input_with_repo}')
+          [[ -f .f4s-update.json ]] && config=$(${jq_bin} "$config * ." .f4s-update.json)
+          params=$(
+            ${nix_bin} flake list-inputs --json | \
+            ${jq_bin} --argjson config "$config" -r '${jq_input_with_repo}'
+          )
           ${nix_bin} flake update $params --commit-lock-file
         '') + /bin/update-repo-inputs.sh;
       };
